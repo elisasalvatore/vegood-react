9e0a2f36876cee336896f5a21033784952f33571
Elisa <elisa.salvatore110@gmail.com>
2025-02-27 16:49:05 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<div className="App">
+				<h1 class="text-3xl font-bold underline hover:text-emerald-500">
+					Hello world!
+				</h1>
+			</div>
+		</>
+	);
+}
+
+export default App;
