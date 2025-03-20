8ae53376a48d4087840854cb7471b7819ab01347
Elisa <elisa.salvatore110@gmail.com>
2025-03-20 12:42:14 +0100

    create: tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				darkGreenCustom: "#17542A",
+				lightGreenCustom: "#9ABC43",
+				paleGreenCustom: "#20B073",
+				mintGreenCustom: "#EEFFF4",
+				blackCustom: "#333333",
+				whiteCustom: "#FFFFFF",
+				beigeCustom: "#F0E9CD",
+				orangeCustom: "#F4901D",
+			},
+		},
+	},
+};
